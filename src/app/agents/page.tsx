28007fc537dc2346d16598476402ac525ab7929f
Jaime <andres_angel@live.com.ar>
2025-02-24 22:26:16 -0500

    docs: :lipstick: Update agent and client pages
    Add Table componet and update filter and query params use

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -1,50 +1,78 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Agent, statusAgent } from "../../../lib/models/agent";
 import { fetchAgents } from "../../../lib/api/agents";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { Table } from "@/components/Table";
+import { AgentFilter } from "../../../lib/models/filter";
 
 export default function AgentsPage() {
     const [agents, setAgents] = useState<Agent[]>([])
+    const pathName = usePathname()
     const searchParams = useSearchParams()
     const router = useRouter()
 
-    const selectedStatus = searchParams.get('status') || ''
+    const filter:AgentFilter = {
+      state: searchParams.get('status') || '',
+      name: searchParams.get('name') || ''
+    } 
 
     useEffect(()=>{
         fetchAgents().then(setAgents)
     },[])
 
-    const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const status = event.target.value
-        router.push(`/agents?status=${status}`)
+    const createQueryString = useCallback(
+      (name: string, value:string) => {
+        const params = new URLSearchParams(searchParams.toString())
+        params.set(name,value)
+
+        return params.toString()
+      },[searchParams]
+    )
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>, param:string) => {
+        const value = event.target.value
+        router.push(`${pathName}?${createQueryString(param, value)}`)
     }
 
-    const filteredAgents = selectedStatus
-      ? agents.filter((agent) => agent.status === selectedStatus)
-      : agents;
+    const verifyFilterAgent = (agent:Agent):boolean=>{
+      const name = agent.name.toLowerCase().includes(filter.name.toLowerCase()) 
+      const status = agent.status === filter.state || !filter.state
+      return name && status
+    }
+
+    const filteredAgents = agents.filter((agent) => verifyFilterAgent(agent))
+     
 
     return (
-      <main className="p-6">
+      <main className="p-6 flex flex-col gap-8">
         <h1 className="text-2xl font-bold">Lista de Agentes</h1>
-
-        <label>
-          Filtrar por estado:
-          <select onChange={handleFilterChange} value={selectedStatus}>
+        <div className="flex flex-wrap w-full gap-5 items-center">
+          <h2>Filtrar por:</h2>
+          
+          <input onChange={(e)=>handleFilterChange(e,'name')} className="w-96" type="text" placeholder="Nombre..."/>
+          
+          <label className="">
+          Estado:
+          <select className="p-1.5 ml-2" onChange={(e)=>handleFilterChange(e,'status')} value={filter.state}>
             <option value=''>Todos</option>
             {statusAgent.map((status, index)=>(
               <option key={index} value={status} className="capitalize">{status}</option>
             ))}
           </select>
-        </label>
+          </label>
+        </div>
 
-        <ul>
-            {filteredAgents.map((agent,index)=>(
-                <li key={index} className="p-2 border-b">
-                    {agent.name} - {agent.status} - {agent.waitTime} min
-                </li>
-            ))}
-        </ul>
+        <Table 
+          headers={['nombre', 'estado','tiempo de espera']} 
+          data={filteredAgents} 
+          renderRow={(agent, odd)=>(
+            <>
+              <td className={`p-1  border-l`}>{agent.name}</td>
+              <td className={`p-1 border-r border-l ${odd&&'border-gray-300'}`}>{agent.status}</td>
+              <td className={`p-1 border-r`}>{agent.waitTime} min</td>
+            </>
+          )} />
       </main>
     );
   }
--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,48 +1,108 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Client } from "../../../lib/models/client";
 import { fetchClients } from "../../../lib/api/clients";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Table } from "@/components/Table";
+import { filterOperations } from "../../../lib/constants/operations";
+import { ClientFilter } from "../../../lib/models/filter";
 
 export default function ClientsPage() {
     const [clients, setClients] = useState<Client[]>([])
+    const pathName = usePathname()
     const searchParams = useSearchParams()
     const router = useRouter()
     
-    const maxWaitTime = searchParams.get('maxWaitTime')||''
+    const filter:ClientFilter = {
+      name: searchParams.get('name') || '',
+      waitTime: searchParams.get('waitTime')||'',
+      operation: searchParams.get('operation') as typeof filterOperations[number] ||'todos'
+    }
 
     useEffect(()=>{
         fetchClients().then(setClients)
     },[])
 
-    const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const createQueryString = useCallback(
+      (name: string, value:string) => {
+        const params = new URLSearchParams(searchParams.toString())
+        params.set(name,value)
+
+        return params.toString()
+      },[searchParams]
+    )
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>, param:string) => {
         const value = event.target.value
-        router.push(`/clients?maxWaitTime=${value}`)
+        router.push(`${pathName}?${createQueryString(param, value)}`)
     }
 
-    const filteredClients = clients
-      ? clients.filter((client)=> client.waitTime <= Number(maxWaitTime) || maxWaitTime === '')
-      : clients
+    const verifyFilterClient = (client: Client):boolean=>{
+      let validWaitTime = true
+      if (filter.waitTime.length>0) {
+        switch (filter.operation) {
+          case 'igual a':
+            validWaitTime = client.waitTime === Number(filter.waitTime)
+            break;
+          
+          case 'menor igual':
+            validWaitTime = client.waitTime <= Number(filter.waitTime)
+            break;
+          
+          case 'mayor igual':
+            validWaitTime = client.waitTime >= Number(filter.waitTime)
+            break;
+        
+          default:
+            break;
+        } 
+      }
+      
+      const validName = client.name.toLowerCase().includes(filter.name.toLocaleLowerCase()) || filter.name === ''
+      
+      return validWaitTime && validName
+    }
+
+    const filteredClients = clients.filter((client) => verifyFilterClient(client))
+      
 
     return (
-      <main className="p-6">
+      <main className="p-6 flex flex-col gap-8">
         <h1 className="text-2xl font-bold">Lista de Clientes</h1>
-        <label className="block mb-4">
-          Filtrar por tiempo de espera:
-          <select className="ml-2 p-2 border rounded" onChange={handleFilterChange} value={maxWaitTime || ""}>
-            <option value="">Todos</option>
-            <option value="5">Menos de 5 min</option>
-            <option value="10">Menos de 10 min</option>
-            <option value="15">Menos de 15 min</option>
-          </select>
-        </label>
-        <ul>
-            {filteredClients.map((client, index)=>(
-                <li key={index} className="p-2 border-b">
-                    {client.name} - {client.waitTime} min
-                </li>
-            ))}
-        </ul>
+        <div className="flex flex-wrap w-full gap-5 items-center">
+          <h2>Filtrar por:</h2>
+          
+          <input onChange={(e)=>handleFilterChange(e, 'name')} className="w-96" type="text" placeholder="Nombre..."/>
+          
+          <div className="flex items-center gap-2">
+            <p>Tiempo de espera:</p>
+            <select onChange={(e)=>handleFilterChange(e,'operation')} className="p-1.5 text-center">
+              {filterOperations.map((operation, index)=>(
+                <option key={index} value={operation==='todos'?'':operation}>{operation}</option>
+              ))}
+            </select>
+
+            <input 
+              onChange={(e)=>handleFilterChange(e, 'waitTime')} 
+              className="w-40" 
+              type="text" 
+              placeholder="Tiempo de espera..."
+              disabled={!filter.operation}/>
+            
+            <p>minutos</p>
+          </div>
+
+        </div>
+
+        <Table 
+          headers={['nombre', 'tiempo de espera']} 
+          data={filteredClients} 
+          renderRow={(client, odd)=>(
+            <>
+              <th className={`p-1 border-l ${odd&&'border-gray-300'}`}>{client.name}</th>
+              <th className={`p-1 border-x ${odd&&'border-gray-300'}`}>{client.waitTime} min</th>
+            </>
+          )} />
       </main>
     );
   }
